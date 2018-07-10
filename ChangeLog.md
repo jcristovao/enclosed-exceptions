50a5bc2d8a1146f664a4ed317d4a943f0cdce74d
Michael Snoyman <michael@snoyman.com>
2018-07-10 19:34:24 +0300

    Skip some tests on GHC 8.4 (fixes #12)

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,9 @@
+# ChangeLog for enclosed-exceptions
+
+## 1.0.3
+
+* Skip some tests on GHC 8.4 [#12](https://github.com/jcristovao/enclosed-exceptions/issues/12)
+
 ## 1.0.2.1
 
 * Support for GHC 7.4 and earlier
--- test/main.hs
+++ test/main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -79,6 +80,7 @@ main = hspec $ do
             it "doesn't catch exceptions lazily thrown in its pure result" $ do
                 tryAny `trierCatchesDeep` False
 
+#if !MIN_VERSION_base(4, 11, 0)
             let shouldBeShow :: Show a => a -> a -> IO ()
                 shouldBeShow x y = show x `shouldBe` show y
 
@@ -93,6 +95,7 @@ main = hspec $ do
                     var <- atomically newEmptyTMVar
                     atomically $ takeTMVar (var :: TMVar ())
                 res `shouldBeShow` Left (toException BlockedIndefinitelyOnSTM)
+#endif
 
         describe "tryDeep" $ do
             it "catches exceptions thrown from the inside" $ do
